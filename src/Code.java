51fcbf084c2a1c3771b3dd18c05e031fcede503e
Sacha Schmid <schmid.sacha@gmail.com>
2017-05-16 16:54:28 +0200

    Add reduce function

--- src/Code.java
+++ src/Code.java
@@ -1,47 +1,67 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Sabakuno on 16.05.17.
  */
 public class Code {
 
-    private int n = 2000;
-    private int k = 2000;
+    private int chainLength = 2000;
+    private int passwords = 2000;
+    private int passwordLength = 7;
 
-    private HashMap<String, String> matches = new HashMap<>(n);
+    private char[] characters = new char[] {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
 
-    public Code(int chainLength, int passwords) {
-        k = passwords;
-        n = chainLength;
+    private HashMap<String, String> matches = new HashMap<>(passwords);
 
+    public Code() {
         generateTable();
     }
 
     private void generateTable() {
         int i = 0;
 
-        while (i < n) {
+        while (i < passwords) {
             int j = 0;
 
             String source = "";
             String result = source;
 
-            while (j < k) {
-                String hashed = h(result);
-                result = reduce(hashed);
+            while (j < chainLength) {
+                BigInteger hashed = h(result);
+                result = reduce(hashed, j);
                 j++;
             }
 
             matches.put(source, result);
+            i++;
         }
     }
-    
-    public String h(String input) {
-        return "";
+
+    public BigInteger h(String input) {
+        return BigInteger.ZERO;
     }
 
-    public String reduce(String hashed) {
-        return "";
+    public String reduce(BigInteger hashed, int level) {
+        hashed = hashed.add(BigInteger.valueOf(level));
+
+        List<BigInteger> results = new ArrayList<>();
+
+        for (int i = 1; i <= passwordLength; i++) {
+            results.add(0, hashed.mod(BigInteger.valueOf(characters.length)));
+            hashed = hashed.divide(BigInteger.valueOf(characters.length));
+        }
+
+        return results.stream()
+                .map(r -> characters[r.intValue()])
+                .map(String::valueOf)
+                .collect(Collectors.joining(""));
     }
 
 }
--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,12 @@
+import java.math.BigInteger;
+
 /**
  * Created by Sabakuno on 16.05.17.
  */
 public class Program {
+
+    public static void main(String[] args) {
+        Code c = new Code();
+    }
+
 }

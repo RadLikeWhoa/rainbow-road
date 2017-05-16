185c022059abe90ba98006aef20226da98b39d59
Sacha Schmid <schmid.sacha@gmail.com>
2017-05-16 16:24:44 +0200

    Add table generation method

--- src/Code.java
+++ src/Code.java
@@ -1,15 +1,47 @@
+import java.util.HashMap;
+
 /**
  * Created by Sabakuno on 16.05.17.
  */
 public class Code {
 
-    // Kettenlänge
+    private int n = 2000;
     private int k = 2000;
 
-    // Hashfunktion
-    public int h() {
+    private HashMap<String, String> matches = new HashMap<>(n);
 
-        return 0;
+    public Code(int chainLength, int passwords) {
+        k = passwords;
+        n = chainLength;
+
+        generateTable();
+    }
+
+    private void generateTable() {
+        int i = 0;
+
+        while (i < n) {
+            int j = 0;
+
+            String source = "";
+            String result = source;
+
+            while (j < k) {
+                String hashed = h(result);
+                result = reduce(hashed);
+                j++;
+            }
+
+            matches.put(source, result);
+        }
+    }
+    
+    public String h(String input) {
+        return "";
+    }
+
+    public String reduce(String hashed) {
+        return "";
     }
 
 }

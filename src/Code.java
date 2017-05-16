e23305e0b829456383f5a6571450a3782bb42268
Sabakuno90 <baumgartner.tobias@gmx.ch>
2017-05-16 16:55:07 +0200

    Added hash function MD5

--- src/Code.java
+++ src/Code.java
@@ -1,15 +1,35 @@
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by Sabakuno on 16.05.17.
  */
 public class Code {
 
     // Kettenlänge
-    private int k = 2000;
+    public int k = 2000;
 
     // Hashfunktion
-    public int h() {
-
-        return 0;
+    public BigInteger h(String input) {
+        MessageDigest md;
+        byte[] output;
+        try {
+            md = MessageDigest.getInstance("MD5");
+            output = md.digest(input.getBytes("UTF-8"));
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < output.length; i++) {
+                sb.append(Integer.toString((output[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            System.out.println(sb.toString());
+            return new BigInteger(output);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return BigInteger.ZERO;
     }
 
 }
--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,16 @@
+import java.math.BigInteger;
+
 /**
  * Created by Sabakuno on 16.05.17.
  */
 public class Program {
+
+    public static void main(String[] args) {
+
+        Code test = new Code();
+        BigInteger toby = test.h("0000000");
+        System.out.println(toby);
+
+    }
+
 }

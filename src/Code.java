cbe820931398987c1cac87d4811dbd6e80b51238
Sabakuno90 <baumgartner.tobias@gmx.ch>
2017-05-16 16:09:24 +0200

    Code files created

---
+++ src/Code.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Sabakuno on 16.05.17.
+ */
+public class Code {
+
+    // Kettenlänge
+    private int k = 2000;
+
+    // Hashfunktion
+    public int h() {
+
+        return 0;
+    }
+
+}
---
+++ src/Program.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Sabakuno on 16.05.17.
+ */
+public class Program {
+}
